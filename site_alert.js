9a4e4be6526d0af50dea56732909cbeabd031b53
cecrs <cecily.borzillo@gmail.com>
2014-09-18 11:43:48 -0500

    Site Alert module code.

---
+++ site_alert.js
@@ -0,0 +1,21 @@
+
+/**
+ * @file site_alert.js
+ */
+(function ($) {
+
+// Initial page load.
+$(window).load(function() {
+  // Load alert text.
+  loadAlert();
+});
+
+// Function to update alert text.
+var loadAlert = function () {
+  var callback = '/ajax/site_alert';
+  $('.site-alert').load(callback);
+  // Update content every 5 minutes.
+  setTimeout(loadAlert, 300000);
+}
+
+})(jQuery);
