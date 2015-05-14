7c65cbd20f11a2794a5369025c95e12fa7507887
bmoresafety20 <bmoresafety20@1175666.no-reply.drupal.org>
2015-05-14 13:20:16 -0700

    Issue #2475823 by kerasai: JavaScript Refactor

--- site_alert.js
+++ site_alert.js
@@ -6,18 +6,22 @@
 
 (function ($) {
 
-// Initial page load.
-$(window).load(function() {
-  // Load alert text.
-  loadAlert();
-});
+var basePath;
 
-// Function to update alert text.
-var loadAlert = function () {
-  var callback = '/ajax/site_alert';
-  $('.site-alert').load(callback);
-  // Update content every 5 minutes.
-  setTimeout(loadAlert, 300000);
-}
+  Drupal.behaviors.siteAlert = {
+    attach: function(context, settings) {
+      basePath = settings.basePath;
+      loadAlert($('.site-alert', context));
+    }
+  };
+
+  // Function to update alert text.
+  var loadAlert = function (alert) {
+    var callback = basePath + 'ajax/site_alert';
+    alert.load(callback);
+
+    // Update content every 5 minutes.
+    setTimeout(function () { loadAlert(alert) }, 300000);
+  }
 
 })(jQuery);
